7744e1f69c51a0c43e58739e9ea49d7cb20a9a17
nadiacom <nadia.comptier@gmail.com>
2014-11-05 15:09:15 +0100

    First release

---
+++ src/com/app/saco/Identification.java
@@ -0,0 +1,20 @@
+package com.app.saco;
+
+import com.example.android.navigationdrawerexample.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Identification extends Fragment {
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+		View rootView = inflater.inflate(R.layout.identification, container, false);
+		return rootView;
+    }
+}
